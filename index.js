0e59caf31b4e18cbcba3d91b4328fcbdc42502bd
marloesverdonk <marloesverdonk@live.nl>
2019-09-06 16:53:51 +0200

    Login form finished

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const imageRouter = require('./image/router')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const authRouter = require('./auth/router')
+const userRouter = require('./user/router')
 
 const port = process.env.PORT || 4000
 const app = express()
@@ -12,8 +14,10 @@ app.use(corsMiddleware)
 const bodyParserMiddleware = bodyParser()
 app.use(bodyParserMiddleware)
 
-
 app.use(imageRouter)
+app.use(authRouter)
+app.use(userRouter)
+
 
 
 app.listen(port, () => console.log(`listening on port ${port}`))
