40c669cc3193a174031ed55534b01389bd3811b2
marloesverdonk <marloesverdonk@live.nl>
2019-09-06 13:45:52 +0200

    Array of rabbits

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const imageRouter = require('./image/router')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const port = process.env.PORT || 4000
-
 const app = express()
+
+const corsMiddleware = cors()
+app.use(corsMiddleware)
+
+const bodyParserMiddleware = bodyParser()
+app.use(bodyParserMiddleware)
+
+
 app.use(imageRouter)
 
 
