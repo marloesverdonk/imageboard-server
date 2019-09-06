bf54948a6f434c35ed1bfdfcf78ed1c160780e6c
marloesverdonk <marloesverdonk@live.nl>
2019-09-06 11:18:33 +0200

    Sequelize

---
+++ db.js
@@ -0,0 +1,12 @@
+const Sequelize = require('sequelize')
+
+const databaseUrl = 
+  process.env.DATABASE_URL ||
+  'postgres://postgres:munchie@localhost:5432/postgres'
+
+const db = new Sequelize(databaseUrl)
+db.sync()
+  .then(() => console.log('Database connected'))
+  .catch(console.error)
+
+module.exports = db
